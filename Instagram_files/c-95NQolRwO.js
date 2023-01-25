6033724f465e46003a935631e3104aafeb8e3b52
Elias Coelho <103864838+escoelho@users.noreply.github.com>
2023-01-24 22:07:04 -0300

    Add files via upload

---
+++ Instagram_files/c-95NQolRwO.js
@@ -0,0 +1,4 @@
+;/*FB_PKG_DELIM*/
+
+__d("IGWebPreCallContext",["react"],(function(a,b,c,d,e,f,g){"use strict";a=d("react").createContext;b=a(null);g["default"]=b}),98);
+__d("XPolarisCallControllerRouteBuilder",["jsRouteBuilder"],(function(a,b,c,d,e,f,g){a=c("jsRouteBuilder")("/call/",Object.freeze({has_video:!0}),void 0);b=a;g["default"]=b}),98);
